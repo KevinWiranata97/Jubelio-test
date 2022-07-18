bf307e9de80d968a5a8c1d45f127647342d2c6f2
KevinWiranata97 <wiranatakevin89@gmail.com>
2022-07-19 01:07:22 +0700

    fitur add products done

--- server/app.js
+++ server/app.js
@@ -1,31 +1,62 @@
-'use strict';
+"use strict";
 
-const Hapi = require('@hapi/hapi');
+const Hapi = require("@hapi/hapi");
+const db = require("./services/index");
 
 const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
-
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-
-            return 'Hello World!';
+  server.route([
+    {
+      method: "GET",
+      path: "/",
+      handler: async (request, h) => {
+        try {
+          const result = await db.query("SELECT * FROM Users");
+          return h.response(result).code(200);
+        } catch (error) {
+          console.log(error);
         }
-    });
+      },
+    },
+    {
+      method: "GET",
+      path: "/products",
+      handler: async (request, h) => {
+        try {
+          const result = await db.query("SELECT * FROM Products");
+          return h.response(result).code(200);
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    },
+    {
+      method: "POST",
+      path: "/products",
+      handler: async (request, h) => {
+        try {
+          const { product_name, image, sku, price, description } = request.payload;
+          await db.query(`INSERT INTO Products(product_name, image, SKU, price, description)
+          values ('${product_name}', '${image}', '${sku}', '${price}', '${description}')`);
+          return h.response("data successfully added").code(201);
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    },
+  ]);
 
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
 };
 
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
 });
 
 init();
