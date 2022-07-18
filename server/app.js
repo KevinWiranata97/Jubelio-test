e97cda7e34476a6477be519b8c5963674bbe98ff
KevinWiranata97 <wiranatakevin89@gmail.com>
2022-07-19 02:20:50 +0700

    CRUD dan register user done

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const Hapi = require("@hapi/hapi");
+const Boom = require("@hapi/boom");
 const db = require("./services/index");
+const bcrypt = require("bcryptjs");
 
 const init = async () => {
   const server = Hapi.server({
@@ -11,12 +13,15 @@ const init = async () => {
 
   server.route([
     {
-      method: "GET",
-      path: "/",
+      method: "POST",
+      path: "/register",
       handler: async (request, h) => {
         try {
-          const result = await db.query("SELECT * FROM Users");
-          return h.response(result).code(200);
+          const { user_name, password, email } = request.payload;
+          const hash = await bcrypt.hash(password, 10);
+          await db.query(`INSERT INTO Users(user_name, password, email)
+          VALUES ('${user_name}', '${hash}', '${{email}}')`);
+          return h.response("Account successfully created").code(201);
         } catch (error) {
           console.log(error);
         }
@@ -27,7 +32,7 @@ const init = async () => {
       path: "/products",
       handler: async (request, h) => {
         try {
-          const result = await db.query("SELECT * FROM Products");
+          const result = await db.query("SELECT * FROM Products")
           return h.response(result).code(200);
         } catch (error) {
           console.log(error);
@@ -39,7 +44,8 @@ const init = async () => {
       path: "/products",
       handler: async (request, h) => {
         try {
-          const { product_name, image, sku, price, description } = request.payload;
+          const { product_name, image, sku, price, description } =
+            request.payload;
           await db.query(`INSERT INTO Products(product_name, image, SKU, price, description)
           values ('${product_name}', '${image}', '${sku}', '${price}', '${description}')`);
           return h.response("data successfully added").code(201);
@@ -48,15 +54,61 @@ const init = async () => {
         }
       },
     },
+    {
+      method: ["PUT", "GET"],
+      path: "/products/{id}",
+      handler: async (request, h) => {
+        try {
+          const id = request.params.id;
+
+          const { product_name, image, sku, price, description } =
+            request.payload;
+          const findProduct = await db.query(
+            `SELECT * FROM Products Where id = '${id}'`
+          );
+          if (findProduct.length === 0) {
+            return Boom.notFound(`Product with id ${id} not found`);
+          }
+
+          await db.query(
+            `UPDATE Products SET product_name = '${product_name}', image = '${image}', sku = '${sku}', price = '${price}', description = '${description}' WHERE id = '${id}'`
+          );
+
+          return h.response("data successfully updated").code(200);
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    },
+    {
+      method: 'DELETE',
+      path: "/products/{id}",
+      handler: async (request, h) => {
+        try {
+          const id = request.params.id;
+          
+          const findProduct = await db.query(
+            `SELECT * FROM Products Where id = '${id}'`
+          );
+          if (findProduct.length === 0) {
+            return Boom.notFound(`Product with id ${id} not found`);
+          }
+
+          await db.query(
+            `DELETE FROM Products WHERE id = '${id}'`
+          );
+
+          return h.response("data successfully deleted").code(200);
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    },
+
   ]);
 
   await server.start();
   console.log("Server running on %s", server.info.uri);
 };
-
-process.on("unhandledRejection", (err) => {
-  console.log(err);
-  process.exit(1);
-});
 
 init();
--- server/services/db.js
+++ server/services/db.js
@@ -1,7 +1,4 @@
 const pg = require("pg");
-
-
-
 const config = {
   user: "postgres", //this is the db user credential
   database: "Jubelio",
